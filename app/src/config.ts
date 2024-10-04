daf5a6d719fc2c2b838d348414a15faa980a8b7e
hackabiel <hackabielbienne@gmail.com>
2024-10-04 12:07:14 +0200

    Onbot-testpages

--- app/src/config.ts
+++ app/src/config.ts
@@ -15,9 +15,9 @@ export default defineConfig({
           },
           shape: 'square',
           pagination: 'count',
-          title: 'Welcome to Telegram Onboarding Kit',
+          title: 'Welcome to La Macherie - hackabiel',
           description:
-            "Create stunning onboarding and paywall for your Telegram Bot using the full power of Mini Apps<br><br>It's <b>simple</b>, <b>fast</b>, highly <b>customizable</b> and <a href='https://github.com/Easterok/telegram-onboarding-kit' target='_blank'>open-source</a>!",
+            "Where creatives, freelancers, and entrepreneurs unite to get things done together.<br><br> It's <b>collaborative</b>, <b>goal-driven</b>, and fully <b>supportive</b> This is your space to turn ideas into action and grow your network.</p>",
           button: 'Next',
         },
 
